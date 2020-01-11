c4b58bd944b015c81525b8175877f76899e2a75c
jiaheng-zhang <jiaheng.zhang@rea-group.com>
2020-01-11 23:19:37 +0800

    :beers: Add 3rd test case and code.

--- src/main/java/com/tw/FizzBuzz.java
+++ src/main/java/com/tw/FizzBuzz.java
@@ -1,9 +1,13 @@
 package com.tw;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FizzBuzz {
-    public static List<Integer> count(List<Integer> numbers) {
-        return numbers;
+    public static List<String> of(List<Integer> numbers) {
+        return numbers.stream().flatMap(
+                num -> Stream.of(num % 3 == 0 ? "Fizz" : String.valueOf(num)))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/tw/FizzBuzzTest.java
+++ src/test/java/com/tw/FizzBuzzTest.java
@@ -11,11 +11,18 @@ public class FizzBuzzTest {
 
     @Test
     void shouldReturn1WhenGiven1() {
-        assertThat(FizzBuzz.count(Collections.singletonList(1))).isEqualTo(Collections.singletonList(1));
+        assertThat(FizzBuzz.of(Collections.singletonList(1))).isEqualTo(Collections.singletonList("1"));
     }
 
     @Test
     void shouldReturn1And2WhenGiven1And2() {
-        assertThat(FizzBuzz.count(Arrays.asList(1, 2))).isEqualTo(Arrays.asList(1, 2));
+        assertThat(FizzBuzz.of(Arrays.asList(1, 2))).isEqualTo(Arrays.asList("1", "2"));
+    }
+
+    @Test
+    void shouldReturnFizzWhenGivenMultipleOf3(){
+        assertThat(FizzBuzz.of(Collections.singletonList(3))).isEqualTo(Collections.singletonList("Fizz"));
+        assertThat(FizzBuzz.of(Collections.singletonList(6))).isEqualTo(Collections.singletonList("Fizz"));
+        assertThat(FizzBuzz.of(Arrays.asList(1, 2, 3, 9))).isEqualTo(Arrays.asList("1", "2", "Fizz", "Fizz"));
     }
 }
